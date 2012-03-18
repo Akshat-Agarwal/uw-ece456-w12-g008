8c61c6e9c11a407ca4ed19093ca2117d36405d32
minsangkim89@gmail.com <minsangkim89@gmail.com@874c316b-525d-6838-2b6c-a76b53066726>
2012-03-18 04:28:10 +0000

    always re-try connection to DB

--- trunk/eclipse/shared/src/org/ece456/proj/orm/objects/Patient.java
+++ trunk/eclipse/shared/src/org/ece456/proj/orm/objects/Patient.java
@@ -38,4 +38,8 @@ public class Patient implements Serializable {
     public PatientMedical getMedical() {
         return medical;
     }
+
+    public String getName() {
+        return contact.getName();
+    }
 }
